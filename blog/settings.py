4ae3c4b744d5bfb50c26d6486f2ace40c6eb072f
Michał Lejwoda <saxatachi@gmail.com>
2021-03-13 17:33:15 +0100

    delete Default_File_Storage

--- blog/settings.py
+++ blog/settings.py
@@ -203,5 +203,5 @@ ADS_VIEWPORTS = {
 }
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
