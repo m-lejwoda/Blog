6d986d720cc4addbe65a897d8ace50e2a2b47cd2
saxatachi <34982715+saxatachi@users.noreply.github.com>
2021-05-04 11:24:59 +0200

    Debug = False

--- blog/settings.py
+++ blog/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c780bnb)h8j9#+nhrdmrzhes#%&u4=lq^lykqaqj-ez6vza+v1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
