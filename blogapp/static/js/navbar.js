9f6d41bb0e9229da946d96bc5adfca48962fd538
saxatachi <34982715+saxatachi@users.noreply.github.com>
2021-05-04 15:20:15 +0200

    clear console.logs

--- blogapp/static/js/navbar.js
+++ blogapp/static/js/navbar.js
@@ -1,11 +1,1 @@
-console.log("navbar")
-if ($('.navbar__mobile-icon'). is(":focus")) {
-    console.log("jest nacisniety")
-}else{
-    console.log("nie jest")
-}
-// console.log($('.navbar__mobile-icon'))
-$(document).ready(function(){
-    console.log("navbar gotowy")
-    
-})
+
