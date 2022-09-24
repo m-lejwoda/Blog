305f0802524cb932c1b018b2d095bb6137802415
michal <m.lejwoda@sunshineprofits.com>
2022-09-24 18:51:56 +0200

    back to django 2.2

--- blog/urls.py
+++ blog/urls.py
@@ -17,12 +17,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blogapp.urls')),
     path('ckeditor/',include('ckeditor_uploader.urls')),
-    path('__debug__/', include('debug_toolbar.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
 
 
