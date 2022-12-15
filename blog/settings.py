5fc5041cb41aeb0192dea7b9b92e452b0e3ab06d
michal <m.lejwoda@sunshineprofits.com>
2022-12-15 12:43:54 +0100

    change settings db to localhost

--- blog/settings.py
+++ blog/settings.py
@@ -119,7 +119,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'db',
+        'HOST': 'localhost',
         'PORT': 5432
     }
 }
