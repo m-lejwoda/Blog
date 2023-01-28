628855367f3ac7ac1a918a2476c325f4a8ad7f11
michal <m.lejwoda@sunshineprofits.com>
2023-01-28 20:05:49 +0100

    dockerFile fix

--- manage.py
+++ manage.py
@@ -5,7 +5,6 @@ import sys
 
 
 def main():
-    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.config.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
